f4d8dbc99465421f2298c6156f57703f4ab645c8
navitanavita339@gmail.com <navitanavita339@gmail.com>
2019-04-13 13:36:48 -0400

    Second Commit

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -8,14 +8,30 @@ namespace BankAccounts
 
 {   /// <summary>
     /// Bank Account demo class 
-    
+
     public class BankAccount
     {
+
         private string m_customerName;
         private double m_balance;
         private bool m_frozen = false;
+
         private BankAccount()
         {
 
         }
+        public BankAccount(String customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+        public double Balance
+        {
+            get { return m_balance; }
+        }
     }
+}
